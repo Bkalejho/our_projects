0a20cba876b6b5bfa32c2d114f9a222d7352b1c1
bkalejho <bkalejho@gmail.com>
2020-01-31 12:19:56 -0500

    just a configuration test on Alejo-Linux-Office

--- gpio/build/freertos/port/esp8266/xtensa_context.d
+++ gpio/build/freertos/port/esp8266/xtensa_context.d
@@ -1,57 +1,57 @@
 port/esp8266/xtensa_context.o: \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/xtensa_context.S \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_rtos.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/coreasm.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/hal.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core-isa.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core-matmap.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/tie.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/tie-asm.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/specreg.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/corebits.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/system.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/simcall.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_context.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/xtruntime-frames.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_timer.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/FreeRTOSConfig.h \
- /home/jonycarmona/our_projects/gpio/build/include/sdkconfig.h \
- /home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_rtos.h
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/xtensa_context.S \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_rtos.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/coreasm.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/hal.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core-isa.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core-matmap.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/tie.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/tie-asm.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/specreg.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/corebits.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/system.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/simcall.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_context.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/xtruntime-frames.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_timer.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/FreeRTOSConfig.h \
+ /home/alejandro/IoThix/ESP8266/gpio/build/include/sdkconfig.h \
+ /home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_rtos.h
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_rtos.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_rtos.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/coreasm.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/coreasm.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/hal.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/hal.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core-isa.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core-isa.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core-matmap.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/core-matmap.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/tie.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/tie.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/tie-asm.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/tie-asm.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/specreg.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/specreg.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/corebits.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/corebits.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/system.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/config/system.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/simcall.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/simcall.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_context.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_context.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/xtruntime-frames.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/esp8266/include/xtensa/xtruntime-frames.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_timer.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_timer.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/FreeRTOSConfig.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/FreeRTOSConfig.h:
 
-/home/jonycarmona/our_projects/gpio/build/include/sdkconfig.h:
+/home/alejandro/IoThix/ESP8266/gpio/build/include/sdkconfig.h:
 
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_rtos.h:
-/home/jonycarmona/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/xtensa_context.S:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/include/freertos/xtensa_rtos.h:
+/home/alejandro/esp/ESP8266_RTOS_SDK/components/freertos/port/esp8266/xtensa_context.S:
