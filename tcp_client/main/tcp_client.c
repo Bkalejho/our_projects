b81f24cf4500cd9a762b9b216d6fac54620b46b5
bkalejho <bkalejho@gmail.com>
2020-02-05 04:41:40 -0500

    chnage the ssid and password

--- tcp_client/main/tcp_client.c
+++ tcp_client/main/tcp_client.c
@@ -28,13 +28,13 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_WIFI_SSID "jony"
-#define EXAMPLE_WIFI_PASS "jfcr920210"
+#define EXAMPLE_WIFI_SSID "HUAWEI P8 Alejho"
+#define EXAMPLE_WIFI_PASS "11235813"
 
 #ifdef CONFIG_EXAMPLE_IPV4
 #define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
 #else
-#define HOST_IP_ADDR "127.0.0.1"
+#define HOST_IP_ADDR "12r7.0.0.1"
 #endif
 
 #define PORT "12345"
